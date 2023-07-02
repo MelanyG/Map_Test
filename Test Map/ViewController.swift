9c7950f28bb8122a8ac75eae1f803a37ddf54077
Melany Gulianovych <melanyhul@gmail.com>
2023-07-02 17:44:26 +0300

    Initial Commit

---
+++ Test Map/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Test Map
+//
+//  Created by Melany Gulianovych on 02.07.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
